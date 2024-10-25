7c65f097fb7dd74b9438085cf43396125387eb71
Markus Keil <markus@keil-connect.com>
2024-10-25 09:35:13 +0200

    add db migrations

---
+++ Migrations/20241025073504_add-stuckreplacement-flag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GithubActionsOrchestrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class addstuckreplacementflag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "StuckJobReplacement",
+                table: "Runners",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StuckJobReplacement",
+                table: "Runners");
+        }
+    }
+}
